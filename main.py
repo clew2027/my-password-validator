a073c60523a8995baf627501db6a4faac0d29b62
Charlotte Lew <clew27@seas.upenn.edu>
2025-03-25 13:04:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,7 @@ def check_password():
     pw = data.get("password", "")
 
     # FIXME: to be implemented
-    return flask.jsonify({"valid": False, "reason": "Not implemented"}), 501
+    if len(pw) < 8:
+        return flask.jsonify({"valid":False, "reason": "Password too short"})
+
+    return flask.jsonify({"valid": True, "reason: "" }), 200
